f60bf8a6a93927a92484e65b3174e77eb8aff8b1
vprotcykovich <protcykovich@gmail.com>
2018-06-08 14:04:48 +0300

    database structure changes

--- server-side/schema/link-bank.sql
+++ server-side/schema/link-bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 03 2018 г., 23:33
+-- Время создания: Июн 08 2018 г., 13:58
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -50,7 +50,8 @@ INSERT INTO `links` (`linkID`, `text`, `url`, `tableID`) VALUES
 
 CREATE TABLE `linkTables` (
   `id` int(11) NOT NULL,
-  `tableName` varchar(255) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `width` int(11) NOT NULL,
   `height` int(11) NOT NULL,
   `x` int(11) NOT NULL,
@@ -61,14 +62,26 @@ CREATE TABLE `linkTables` (
 -- Дамп данных таблицы `linkTables`
 --
 
-INSERT INTO `linkTables` (`id`, `tableName`, `width`, `height`, `x`, `y`) VALUES
-(1, 'sport', 200, 200, 300, 300),
-(2, 'school', 300, 300, 200, 200),
-(3, 'job', 400, 500, 100, 100),
-(5, 'job', 400, 500, 100, 100),
-(6, 'job', 400, 500, 100, 100),
-(7, 'job', 400, 500, 100, 100),
-(8, 'hobby', 50, 50, 120, 130);
+INSERT INTO `linkTables` (`id`, `userID`, `name`, `width`, `height`, `x`, `y`) VALUES
+(1, 0, 'sport', 200, 200, 300, 300),
+(2, 0, 'school', 300, 300, 200, 200),
+(3, 0, 'job', 400, 500, 100, 100),
+(5, 0, 'job', 400, 500, 100, 100),
+(6, 0, 'job', 400, 500, 100, 100),
+(7, 0, 'job', 400, 500, 100, 100),
+(8, 0, 'hobby', 50, 50, 120, 130);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
@@ -87,6 +100,12 @@ ALTER TABLE `linkTables`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -94,13 +113,19 @@ ALTER TABLE `linkTables`
 -- AUTO_INCREMENT для таблицы `links`
 --
 ALTER TABLE `links`
-  MODIFY `linkID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `linkID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `linkTables`
 --
 ALTER TABLE `linkTables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
